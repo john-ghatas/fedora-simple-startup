2028bbeb15dca34720796baecc6a0ca89a3098a9
John Ghatas <johnghatas97@hotmail.com>
2022-01-20 14:21:21 +0100

    feat(script): Add initial version of the script

---
+++ setup-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install thunderbird wget rstudio okular zsh gnome-tweaks gnome-shell-extension-dash-to-dock gnome-extensions-app -y
+# Install RPMFusion
+echo "Installing multimedia libraries......"
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+
+# Install VSCodium
+echo "Installing VSCode......"
+sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf install codium -y
+
+# Enable fractional scaling
+echo "Enable fractional scaling......"
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+
+# Init flatpak
+echo "Enabling non-free Flatpak repository for extra software compatiblity......"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
