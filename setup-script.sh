6a79bd80c6a37ed83591faf8b78f799199de6346
John Ghatas <johnghatas97@hotmail.com>
2022-01-21 21:49:15 +0100

    fix(flatpak): Add override for home dir

--- setup-script.sh
+++ setup-script.sh
@@ -28,3 +28,4 @@ gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffe
 echo "Enabling non-free Flatpak repository for extra software compatiblity......"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak override --user --filesystem=~/.themes
+flatpak override --user --filesystem=$HOME
