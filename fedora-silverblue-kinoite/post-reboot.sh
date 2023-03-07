c3cb0f8f20176060fcdde6e64a3cd1e39ecfffa5
John Ghatas <johnghatas97@hotmail.com>
2023-03-07 03:14:19 +0100

    fixup: Remove sudo from package installs with rpm-ostree

--- fedora-silverblue-kinoite/post-reboot.sh
+++ fedora-silverblue-kinoite/post-reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting up packages" 
-sudo rpm-ostree install distrobox docker ffmpeg gnome-tweaks gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi mesa-va-drivers-freeworld neovim tilix zsh 
-sudo rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
+rpm-ostree install distrobox docker ffmpeg gnome-tweaks gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi mesa-va-drivers-freeworld neovim tilix zsh 
+rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
 
 
 echo "Adding permissions for docker" 
@@ -10,6 +10,6 @@ sudo runuser -l root -c "usermod -aG docker $USER"
 
 echo "Install the hardware codecs for your system"
 echo "AMD: rpm-ostree override remove mesa-vdpau-drivers --install mesa-vdpau-drivers-freeworld"
-echo "Intel: sudo rpm-ostree install intel-media-driver"
+echo "Intel: rpm-ostree install intel-media-driver"
 
 echo "Please reboot again to make the changes final" 
