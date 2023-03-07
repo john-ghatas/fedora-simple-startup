4cc89696bf115957adf9b42a9b15c39d3bdc6b6d
John Ghatas <johnghatas97@hotmail.com>
2023-03-07 01:03:02 +0100

    fix: Remove niche program

--- fedora-silverblue-kinoite/post-reboot.sh
+++ fedora-silverblue-kinoite/post-reboot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Setting up packages" 
-sudo rpm-ostree install distrobox docker ffmpeg gnome-tweaks gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi mesa-va-drivers-freeworld neovim piper tilix zsh 
+sudo rpm-ostree install distrobox docker ffmpeg gnome-tweaks gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi mesa-va-drivers-freeworld neovim tilix zsh 
 sudo rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
+
 
 echo "Adding permissions for docker" 
 sudo runuser -l root -c "grep -E '^docker:' /usr/lib/group >> /etc/group"
